a3e0c642cc6fe3e4c2329b8447f38ee64cf3581b
Rebecca F <rebecca.fsq011@protonmail.com>
2024-10-29 12:20:10 +0100

    :hammer: added a function to retrieve posts

--- frontend/src/js/api.js
+++ frontend/src/js/api.js
@@ -1,7 +1,7 @@
 // Fonction pour ajouter un Post
 
 async function postUser(userData) {
-  const response = await fetch("http://localhost:3000/api/users", {
+  const response = await fetch("http://localhost:4000/api/users", {
     method: "POST",
     body: userData,
   });
@@ -10,5 +10,14 @@ async function postUser(userData) {
     throw new Error("Erreur réseau lors de l'envoi de l'utilisateur");
   }
 
+  return response.json();
+}
+
+// Fonction pour récupérer les posts
+async function getPosts() {
+  const response = await fetch("http://localhost:4000/api/posts");
+  if (!response.ok) {
+    throw new Error("Erreur lors de la récupération des posts");
+  }
   return response.json();
 }
